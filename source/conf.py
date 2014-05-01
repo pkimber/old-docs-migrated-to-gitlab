0b2b53d14e2064fc54d918af783a89ef9640c53a
Patrick Kimber <code@pkimber.net>
2014-05-01 13:40:28 +0100

    does not work with read the docs

--- source/conf.py
+++ source/conf.py
@@ -11,7 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sphinx_rtd_theme
+# does not work with read the docs!
+# import sphinx_rtd_theme
 import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -92,9 +93,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'nature'
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'nature'
+
+# does not work with read the docs!
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
